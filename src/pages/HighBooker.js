3ef4daeca450edd0dca047c028fb3adfa268ec50
Taekwan Park <techigh@Taekwans-MacBook-Pro.local>
2022-05-02 14:13:34 +0900

    portfolio video

--- src/pages/HighBooker.js
+++ src/pages/HighBooker.js
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 import { Helmet } from "react-helmet";
 import Layout from "../components/Layout";
-import one from "../assets/img/portfolio/1.png";
-// import two from "../assets/img/portfolio/2.png";
-import three from "../assets/img/portfolio/3.png";
-import four from "../assets/img/portfolio/4.png";
-import five from "../assets/img/portfolio/5.png";
-import six from "../assets/img/portfolio/6.png";
-import seven from "../assets/img/portfolio/7.png";
-import eight from "../assets/img/portfolio/8.png";
-import video from "../assets/img/portfolio/highmarket.mp4";
+import homei from "../assets/img/portfolio/porthome.png";
+import homev from "../assets/img/portfolio/porthome.mp4";
+import worksi from "../assets/img/portfolio/portwokrs.png";
+import worksv from "../assets/img/portfolio/portwokrs.mp4";
+import abouti from "../assets/img/portfolio/portabout.png";
+import aboutv from "../assets/img/portfolio/portabout.mp4";
+import skillsi from "../assets/img/portfolio/portskills.png";
+import skillsv from "../assets/img/portfolio/portskills.mp4";
+import highmarketi from "../assets/img/portfolio/porthighmarket.png";
+import highmarketv from "../assets/img/portfolio/porthighmarket.mp4";
 import {
   AboutContainer,
   BodyTitle,
@@ -22,22 +23,21 @@ import {
   Container,
   Wrapper,
 } from "./HighMarket";
+import { useState } from "react";
 
 const Portfolio = () => {
   const itemDatas = [
-    { src: one },
-    { vsrc: video },
-    { src: three },
-    { src: four },
-    { src: five },
-    { src: six },
-    { src: seven },
-    { src: eight },
+    { src: homei, vsrc: homev },
+    { src: worksi, vsrc: worksv },
+    { src: abouti, vsrc: aboutv },
+    { src: skillsi, vsrc: skillsv },
+    { src: highmarketi, vsrc: highmarketv },
   ];
 
   const onClick = () => {
     window.open("https://high-market.vercel.app/enter", "_blank");
   };
+  const [current, setCurrent] = useState(null);
 
   return (
     <Layout>
@@ -76,24 +76,27 @@ const Portfolio = () => {
           {itemDatas.map((item, idx) => (
             <Item key={idx}>
               <ImageContainer>
-                {item.vsrc ? (
-                  <Video
-                    // width={500}
-                    // onMouseOver={e => e.target.play()}
-                    // onMouseOut={e => e.target.pause()}
-                    autoPlay={true}
-                    muted="muted"
-                  >
-                    <source src={video} type="video/mp4" />
-                  </Video>
-                ) : (
-                  <Image src={item.src} />
-                )}
+                <Video
+                  // width={500}
+                  onMouseOver={e => {
+                    e.target.play();
+                  }}
+                  onMouseOut={e => {
+                    e.target.pause();
+                  }}
+                  // autoPlay={true}
+                  muted="muted"
+                  state={idx === current}
+                >
+                  <source src={item.vsrc} type="video/mp4" />
+                </Video>
+                <Image
+                  src={item.src}
+                  onMouseOver={() => setCurrent(idx)}
+                  // onMouseOut={() => setCurrent(null)}
+                  state={idx === current}
+                />
               </ImageContainer>
-              <Background id="background" />
-              <Back>
-                <Text>abcdef</Text>
-              </Back>
             </Item>
           ))}
         </Slider>
@@ -148,16 +151,14 @@ const Slider = styled.div`
 const Item = styled.div`
   --radius-1: 1rem;
   --bgColor-back: #1e1e1e;
-  width: 280px;
-  height: 280px;
-  /* transition: 300ms; */
+  width: 300px;
+  height: 300px;
   box-shadow: 5px 20px 25px -5px rgb(0 0 0 / 1), 0 8px 10px -6px rgb(0 0 0 / 1);
   display: flex;
   justify-content: center;
   position: relative;
 
   :hover {
-    /* overflow: unset; */
     z-index: 10;
     box-shadow: none;
     div {
@@ -167,43 +168,34 @@ const Item = styled.div`
 
       video {
         width: 560px;
-        /* height: 290px; */
         transition: transform 200ms linear;
         transform: scale(1.1);
         border-radius: var(--radius-1);
         border: 2px solid gray;
       }
+
       img {
         width: 560px;
-        /* height: 290px; */
         transition: transform 200ms linear;
         transform: scale(1.1);
         border-radius: var(--radius-1);
         border: 2px solid gray;
       }
-    }
-    #background {
-      /* transition: transform 300ms cubic-bezier(0.21, 1.06, 0.81, 1.01),
-        opacity 100ms linear;
-      transform: scale(2, 1.68);
-      opacity: 1; */
     }
   }
 
   @media screen and (max-width: 1200px) {
     :hover {
+      transform: translateY(10%);
       div {
         video {
-          width: 480px;
+          width: 500px;
           height: 260px;
         }
         img {
-          width: 480px;
+          width: 500px;
           height: 260px;
         }
-      }
-      #background {
-        /* transform: scale(1.4, 1.5); */
       }
     }
   }
@@ -220,48 +212,42 @@ const Item = styled.div`
           height: 230px;
         }
       }
-      #background {
-        /* transform: scale(1.4, 1.5); */
-      }
     }
   }
 `;
 const ImageContainer = styled.div`
+  position: relative;
   height: max-content;
   transition-property: transform;
   transition-duration: 100ms;
-  /* border: 1px solid green; */
-
   box-shadow: 5px 20px 25px -5px rgb(0 0 0 / 1), 0 8px 10px -6px rgb(0 0 0 / 1);
 `;
 
 const Video = styled.video`
-  width: 280px;
-  height: 280px;
-
+  width: 300px;
+  height: 300px;
   object-fit: cover;
+  display: ${props => (props.state ? "block" : "none")};
 `;
 const Image = styled.img`
-  --ratio: 1.89;
-
-  width: 280px;
-  height: 280px;
-  /* border-radius: var(--radius-1); */
+  width: 300px;
+  height: 300px;
   object-fit: cover;
+  display: ${props => (props.state ? "none" : "block")};
 `;
-const Background = styled.div`
-  position: absolute;
-  inset: 0;
-  background-color: var(--bgColor-back);
-  z-index: -1;
-  transform: scale(0.5);
-  border-radius: 10px;
-  opacity: 0;
-  box-shadow: 5px 20px 25px -5px rgb(0 0 0 / 1), 0 8px 10px -6px rgb(0 0 0 / 1);
-`;
-const Back = styled.div`
-  display: none;
-`;
-const Text = styled.div``;
+// const Background = styled.div`
+//   position: absolute;
+//   inset: 0;
+//   background-color: var(--bgColor-back);
+//   z-index: -1;
+//   transform: scale(0.5);
+//   border-radius: 10px;
+//   opacity: 0;
+//   box-shadow: 5px 20px 25px -5px rgb(0 0 0 / 1), 0 8px 10px -6px rgb(0 0 0 / 1);
+// `;
+// const Back = styled.div`
+//   display: none;
+// `;
+// const Text = styled.div``;
 
 export default Portfolio;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,68 +1,50 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 import { Helmet } from "react-helmet";
 import Layout from "../components/Layout";
-import one from "../assets/img/portfolio/1.png";
-import two from "../assets/img/portfolio/2.png";
-import three from "../assets/img/portfolio/3.png";
-import four from "../assets/img/portfolio/4.png";
-import five from "../assets/img/portfolio/5.png";
-import six from "../assets/img/portfolio/6.png";
-import seven from "../assets/img/portfolio/7.png";
-import eight from "../assets/img/portfolio/8.png";
-import {
-  AboutContainer,
-  BodyTitle,
-  DetailBody,
-  DetailBodyText,
-  DetailBodyTextContainer,
-  DetailBodyTitle,
-  Title,
-  TitleContainer,
-  Container,
-  Wrapper,
-} from "./HighMarket";
-import { useEffect, useRef } from "react";
+
+// import {
+//   AboutContainer,
+//   BodyTitle,
+//   DetailBody,
+//   DetailBodyText,
+//   DetailBodyTextContainer,
+//   DetailBodyTitle,
+//   Title,
+//   TitleContainer,
+//   Container,
+//   Wrapper,
+// } from "./HighMarket";
+// import { useEffect, useRef } from "react";
 
 const Portfolio = () => {
-  const itemDatas = [
-    { src: one },
-    { src: two },
-    { src: three },
-    { src: four },
-    { src: five },
-    { src: six },
-    { src: seven },
-    { src: eight },
-  ];
+  // const onClick = () => {
+  //   window.open("https://high-market.vercel.app/enter", "_blank");
+  // };
 
-  const onClick = () => {
-    window.open("https://high-market.vercel.app/enter", "_blank");
-  };
+  // const boxref = useRef([]);
 
-  const boxref = useRef([]);
+  // function intersectionCallback(entries, observer) {
+  //   entries.forEach(entry => {
+  //     entry.target.classList.toggle("show", entry.isIntersecting);
+  //     // if (entry.intersectionRatio !== 0.7) observer.unobserve(entry.target);
+  //   });
+  // }
 
-  function intersectionCallback(entries, observer) {
-    entries.forEach(entry => {
-      entry.target.classList.toggle("show", entry.isIntersecting);
-      // if (entry.intersectionRatio !== 0.7) observer.unobserve(entry.target);
-    });
-  }
+  // useEffect(() => {
+  //   var intersectionOptions = {
+  //     root: null, // .container class를 가진 엘리먼트를 root로 설정. null일 경우 브라우저 viewport
+  //     rootMargin: "300px -200px 300px -20px", // rootMargin을 '10px 10px 10px 10px'로 설정
+  //     threshold: 1,
+  //   };
+  //   var io = new IntersectionObserver(
+  //     intersectionCallback,
+  //     intersectionOptions
+  //   );
 
-  useEffect(() => {
-    var intersectionOptions = {
-      root: null, // .container class를 가진 엘리먼트를 root로 설정. null일 경우 브라우저 viewport
-      rootMargin: "300px -200px 300px -20px", // rootMargin을 '10px 10px 10px 10px'로 설정
-      threshold: 1,
-    };
-    var io = new IntersectionObserver(
-      intersectionCallback,
-      intersectionOptions
-    );
-
-    boxref.current.forEach(el => {
-      io.observe(el);
-    });
-  }, []);
+  //   boxref.current.forEach(el => {
+  //     io.observe(el);
+  //   });
+  // }, []);
 
   // const [show, setShow] = useState(false);
 
@@ -86,7 +68,7 @@ const Portfolio = () => {
         <title>Portfolio | TAE KWAN</title>
       </Helmet>
 
-      <AboutContainer>
+      {/* <AboutContainer>
         <TitleContainer>
           <Title onClick={onClick}>Portfolio</Title>
         </TitleContainer>
@@ -124,118 +106,118 @@ const Portfolio = () => {
             </ItemContainer>
           ))}
         </Slider>
-      </AboutContainer>
+      </AboutContainer> */}
     </Layout>
   );
 };
 
-const DetailBodyPort = styled(DetailBody)`
-  --radius-1: 1rem;
-  width: 100%;
-  max-width: 1000px;
-  margin-bottom: 130px;
-  background-color: #1e1e1e;
-  border-radius: var(--radius-1);
-  /* border: 1px solid blue; */
+// const DetailBodyPort = styled(DetailBody)`
+//   --radius-1: 1rem;
+//   width: 100%;
+//   max-width: 1000px;
+//   margin-bottom: 130px;
+//   background-color: #1e1e1e;
+//   border-radius: var(--radius-1);
+//   /* border: 1px solid blue; */
 
-  @media screen and (max-width: 1000px) {
-    margin: 0px 20px 130px;
-    height: 440px;
-  }
-`;
-const DetailBodyTextConatinerPort = styled(DetailBodyTextContainer)`
-  max-width: 1000px;
-  /* bottom: 180px; */
+//   @media screen and (max-width: 1000px) {
+//     margin: 0px 20px 130px;
+//     height: 440px;
+//   }
+// `;
+// const DetailBodyTextConatinerPort = styled(DetailBodyTextContainer)`
+//   max-width: 1000px;
+//   /* bottom: 180px; */
 
-  grid-template-columns: repeat(2, minmax(0, 1fr));
-  @media screen and (max-width: 700px) {
-    grid-template-columns: repeat(1, minmax(0, 1fr));
-  }
-`;
-const Slider = styled.div`
-  --spacer: 1rem;
-  --radius-1: 1rem;
-  /* border: 2px solid orange; */
-  display: grid;
-  gap: 10%;
-  grid-auto-flow: column;
-  grid-auto-columns: 50%;
-  margin-bottom: 200px;
-  width: 100%;
+//   grid-template-columns: repeat(2, minmax(0, 1fr));
+//   @media screen and (max-width: 700px) {
+//     grid-template-columns: repeat(1, minmax(0, 1fr));
+//   }
+// `;
+// const Slider = styled.div`
+//   --spacer: 1rem;
+//   --radius-1: 1rem;
+//   /* border: 2px solid orange; */
+//   display: grid;
+//   gap: 10%;
+//   grid-auto-flow: column;
+//   grid-auto-columns: 50%;
+//   margin-bottom: 200px;
+//   width: 100%;
 
-  padding-left: 15%;
-  padding-right: 15%;
+//   padding-left: 15%;
+//   padding-right: 15%;
 
-  overflow-x: auto;
-  overscroll-behavior-inline: contain;
+//   overflow-x: auto;
+//   overscroll-behavior-inline: contain;
 
-  scroll-snap-type: inline mandatory;
-  /* scroll-padding-inline: 1rem;  */
-  /* background-color: red; */
-  div {
-    border: 1px solid green;
-    scroll-snap-align: center;
-    scroll-snap-stop: always;
-  }
-  /* .show {
-    border: 2px solid white;
-    opacity: 1;
-    transform-origin: bottom;
-  } */
+//   scroll-snap-type: inline mandatory;
+//   /* scroll-padding-inline: 1rem;  */
+//   /* background-color: red; */
+//   div {
+//     border: 1px solid green;
+//     scroll-snap-align: center;
+//     scroll-snap-stop: always;
+//   }
+//   /* .show {
+//     border: 2px solid white;
+//     opacity: 1;
+//     transform-origin: bottom;
+//   } */
 
-  @media screen and (max-width: 1000px) {
-    overflow-x: scroll;
-    padding-left: 20px;
-    padding-right: 20px;
-  }
-`;
+//   @media screen and (max-width: 1000px) {
+//     overflow-x: scroll;
+//     padding-left: 20px;
+//     padding-right: 20px;
+//   }
+// `;
 
-const ItemContainer = styled.div`
-  /* border: 1px solid white; */
-  height: 700px;
-  /* width: 1000px; */
-  /* width: 100%; */
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-`;
+// const ItemContainer = styled.div`
+//   /* border: 1px solid white; */
+//   height: 700px;
+//   /* width: 1000px; */
+//   /* width: 100%; */
+//   display: flex;
+//   justify-content: center;
+//   align-items: flex-end;
+// `;
 
-const Item = styled.div`
-  --bgColor-back: #1e1e1e;
-  width: 50%;
-  display: grid;
-  grid-template-rows: min-content;
-  gap: var(--spacer);
-  padding: 50px 50px 100px;
+// const Item = styled.div`
+//   --bgColor-back: #1e1e1e;
+//   width: 50%;
+//   display: grid;
+//   grid-template-rows: min-content;
+//   gap: var(--spacer);
+//   padding: 50px 50px 100px;
 
-  background-color: var(--bgColor-back);
-  border-radius: var(--radius-1);
+//   background-color: var(--bgColor-back);
+//   border-radius: var(--radius-1);
 
-  transition: 200ms;
-  opacity: 0.2;
+//   transition: 200ms;
+//   opacity: 0.2;
 
-  box-shadow: 5px 20px 25px -5px rgb(0 0 0 / 1), 0 8px 10px -6px rgb(0 0 0 / 1);
-  @media screen and (max-width: 1000px) {
-    padding: 20px 20px 100px;
-  }
-`;
-const ImageContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  border-radius: var(--radius-1);
-  overflow: hidden;
-  transition-property: transform;
-  transition-duration: 100ms;
-  box-shadow: 5px 20px 25px -5px rgb(0 0 0 / 1), 0 8px 10px -6px rgb(0 0 0 / 1);
-  /* :hover {
-    transform: scale(1.02);
-  } */
-`;
-const Image = styled.img`
-  --ratio: 1.89;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
+//   box-shadow: 5px 20px 25px -5px rgb(0 0 0 / 1), 0 8px 10px -6px rgb(0 0 0 / 1);
+//   @media screen and (max-width: 1000px) {
+//     padding: 20px 20px 100px;
+//   }
+// `;
+// const ImageContainer = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   border-radius: var(--radius-1);
+//   overflow: hidden;
+//   transition-property: transform;
+//   transition-duration: 100ms;
+//   box-shadow: 5px 20px 25px -5px rgb(0 0 0 / 1), 0 8px 10px -6px rgb(0 0 0 / 1);
+//   /* :hover {
+//     transform: scale(1.02);
+//   } */
+// `;
+// const Image = styled.img`
+//   --ratio: 1.89;
+//   width: 100%;
+//   height: 100%;
+//   object-fit: cover;
+// `;
 
 export default Portfolio;
